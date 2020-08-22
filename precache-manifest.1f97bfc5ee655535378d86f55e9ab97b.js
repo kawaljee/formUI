6e34abb573eaae0cc089e9e64f9a08c1b61c5311
SKawal <kawaljeet.ravi@gmail.com>
2020-08-22 17:04:41 +0530

    Updates

---
+++ precache-manifest.1f97bfc5ee655535378d86f55e9ab97b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad00d7c3f1d3e82b8478a4b27da0f30",
+    "url": "/formUI/index.html"
+  },
+  {
+    "revision": "46e41daeca2b41e89e0b",
+    "url": "/formUI/static/css/main.838a69de.chunk.css"
+  },
+  {
+    "revision": "cd396c2f5e1a004fb5b6",
+    "url": "/formUI/static/js/2.46be6af0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/formUI/static/js/2.46be6af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46e41daeca2b41e89e0b",
+    "url": "/formUI/static/js/main.05a75c88.chunk.js"
+  },
+  {
+    "revision": "da345ebaedff5853d5fa",
+    "url": "/formUI/static/js/runtime-main.b9b033f9.js"
+  },
+  {
+    "revision": "f57984fd6edd0a9dc5ab5d78a2d8976b",
+    "url": "/formUI/static/media/university.f57984fd.png"
+  }
+]);
